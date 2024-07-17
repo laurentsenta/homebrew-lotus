b70ab6785c04930b2acf6c41131b67e850864126
laurentsenta <1136669+laurentsenta@users.noreply.github.com>
2024-07-17 13:33:19 +0000

    Generated Homebrew formulas for the latest release (node=v1.28.0, miner=miner/v1.28.0).

--- Formula/lotus-miner.rb
+++ Formula/lotus-miner.rb
@@ -1,15 +1,15 @@
-This file was generated - DO NOT EDIT
+# This file was generated - DO NOT EDIT
 class LotusMiner < Formula
   desc "A homebrew cask for installing the Lotus miner from filecoin-project/lotus"
   homepage "https://filecoin.io"
-  version "1.27.1"
+  version "1.28.0"
   license "MIT"
 
   depends_on "hwloc"
 
   on_macos do
-    url "https://github.com/galargh/lotus/releases/download/miner/v1.27.1/lotus-miner_v1.27.1_darwin_all.tar.gz"
-    sha256 "some"
+    url "https://github.com/galargh/lotus/releases/download/miner/v1.28.0/lotus-miner_v1.28.0_darwin_all.tar.gz"
+    sha256 "0688d6c829a10639c9608062124272cdbe404cc45833912e49ca74070405c315"
 
     def install
       bin.install "lotus-miner"
@@ -20,8 +20,8 @@ class LotusMiner < Formula
   on_linux do
     on_intel do
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/galargh/lotus/releases/download/miner/v1.27.1/lotus-miner_v1.27.1_linux_amd64_v1.tar.gz"
-        sha256 "some"
+        url "https://github.com/galargh/lotus/releases/download/miner/v1.28.0/lotus-miner_v1.28.0_linux_amd64_v1.tar.gz"
+        sha256 "b771f22f62dee0ab2da1f86966e2ea28e3e47c305d6109449fcd0d949e8b6304"
 
         def install
           bin.install "lotus-miner"
--- Formula/lotus.rb
+++ Formula/lotus.rb
@@ -1,4 +1,4 @@
-This file was generated - DO NOT EDIT
+# This file was generated - DO NOT EDIT
 class Lotus < Formula
   desc "A homebrew cask for installing the Lotus project from filecoin-project/lotus"
   homepage "https://filecoin.io"
@@ -7,9 +7,13 @@ class Lotus < Formula
   depends_on "lotus-node"
   depends_on "lotus-miner"
 
+  def install
+    # No installation needed, only dependencies
+  end
+
   test do
-    system "#{bin}/lotus", "--version"
-    system "#{bin}/lotus-miner --version"
-    system "#{bin}/lotus-worker --version"
+    system "#{Formula["lotus-node"].opt_bin}/lotus", "--version"
+    system "#{Formula["lotus-miner"].opt_bin}/lotus-miner", "--version"
+    system "#{Formula["lotus-node"].opt_bin}/lotus-worker", "--version"
   end
 end
