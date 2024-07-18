22dda46c95157c52cdc863ab25ce7e208ec28252
laurentsenta <1136669+laurentsenta@users.noreply.github.com>
2024-07-18 00:16:14 +0000

    Generated Homebrew formulas for the latest release (node=v1.28.1, miner=miner/v1.28.1).

--- Formula/lotus.rb
+++ Formula/lotus.rb
@@ -2,14 +2,14 @@
 class Lotus < Formula
   desc "A homebrew cask for installing the Lotus node from filecoin-project/lotus"
   homepage "https://filecoin.io"
-  version "1.27.1"
+  version "1.28.1"
   license "MIT"
 
   depends_on "hwloc"
 
   on_macos do
-    url "https://github.com/galargh/lotus/releases/download/v1.27.1/lotus_v1.27.1_darwin_all.tar.gz"
-    sha256 "some"
+    url "https://github.com/galargh/lotus/releases/download/v1.28.1/lotus_v1.28.1_darwin_all.tar.gz"
+    sha256 "0e5941d1466e55b0670fbece4022d965488b2da6969353e07b6fd88cef232815"
 
     def install
       bin.install "lotus"
@@ -19,8 +19,8 @@ class Lotus < Formula
   on_linux do
     on_intel do
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/galargh/lotus/releases/download/v1.27.1/lotus_v1.27.1_linux_amd64_v1.tar.gz"
-        sha256 "some"
+        url "https://github.com/galargh/lotus/releases/download/v1.28.1/lotus_v1.28.1_linux_amd64_v1.tar.gz"
+        sha256 "f4bace4d424f48ad5897964865b765669d6adec85b4462c1dd54c2fee55caa84"
 
         def install
           bin.install "lotus"
